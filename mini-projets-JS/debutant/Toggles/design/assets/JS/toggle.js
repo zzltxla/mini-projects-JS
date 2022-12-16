fcd1c1fb1b6ff8d0bddde36de339a59ca052e075
zzltxla <ata@edenschool.fr>
2022-12-16 11:59:00 +0100

    second commit

--- mini-projets-JS/debutant/Toggles/design/assets/JS/toggle.js
+++ mini-projets-JS/debutant/Toggles/design/assets/JS/toggle.js
@@ -1,12 +1,19 @@
 let btn = document.querySelectorAll("button");
 let backbtn = document.querySelectorAll(".switch");
+
 //VAR REGION ENDS HERE 
 
-function movingBtn () {
-    btn.classList.toggle("LeftToRight");
-    backbtn.classList.toggle("changeColor");
+function movingBtn() {
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].classList.toggle (".LeftToRight");
+        // console.log(btn.forEach(element => i.indexOf(element)));
+        for(let e = 0; e < backbtn.length; e++){
+            backbtn[e].classList.toggle(".changeColor");
+        }
+    }
 }
 
-for (let i = 0; i < btn.length; i++) {
+for(let i = 0; i < btn.length; i++){
     btn[i].addEventListener('click', movingBtn);
 }
+
